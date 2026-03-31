463a53a23e382a185050e4c3f18a17c7d1aa115d
Petros Pissias <petros.pissias@esa.int>
2026-03-31 11:58:08 +0200

    enhancements

---
+++ src/test/java/eu/startales/spacepixels/util/ImageProcessingTest.java
@@ -0,0 +1,37 @@
+package eu.startales.spacepixels.util;
+
+import io.github.ppissias.jtransient.config.DetectionConfig;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+public class ImageProcessingTest {
+
+    @Test
+    public void createsEphemeralConfigAndClampsSlowMoverFractionBelowMaximumForFiveFrames() {
+        DetectionConfig baseConfig = new DetectionConfig();
+        baseConfig.enableSlowMoverDetection = true;
+        baseConfig.slowMoverStackMiddleFraction = 0.75;
+
+        DetectionConfig effectiveConfig = ImageProcessing.createEffectiveDetectionConfig(baseConfig, 5);
+
+        assertNotSame(baseConfig, effectiveConfig);
+        assertEquals(0.75, baseConfig.slowMoverStackMiddleFraction, 0.0);
+        assertEquals(0.60, effectiveConfig.slowMoverStackMiddleFraction, 1.0e-10);
+        assertEquals(4, ImageProcessing.computeSlowMoverStackOrderIndex(5, baseConfig.slowMoverStackMiddleFraction));
+        assertEquals(3, ImageProcessing.computeSlowMoverStackOrderIndex(5, effectiveConfig.slowMoverStackMiddleFraction));
+    }
+
+    @Test
+    public void leavesAlreadySafeSlowMoverFractionUntouched() {
+        DetectionConfig baseConfig = new DetectionConfig();
+        baseConfig.enableSlowMoverDetection = true;
+        baseConfig.slowMoverStackMiddleFraction = 0.60;
+
+        DetectionConfig effectiveConfig = ImageProcessing.createEffectiveDetectionConfig(baseConfig, 7);
+
+        assertEquals(0.60, effectiveConfig.slowMoverStackMiddleFraction, 1.0e-10);
+        assertEquals(5, ImageProcessing.computeSlowMoverStackOrderIndex(7, effectiveConfig.slowMoverStackMiddleFraction));
+    }
+}
